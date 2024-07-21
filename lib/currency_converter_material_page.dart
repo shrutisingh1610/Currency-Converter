b05d475b36e7ba2edf330861e5b791d738c682d0
shrutisingh1610 <shrutisingh161003@gmail.com>
2024-07-21 22:33:53 +0530

    updated layout

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,50 +1,76 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class CurrencyConverterMaterialPage extends StatelessWidget {
-  const CurrencyConverterMaterialPage({super.key}); // 1
+  // Removed the const keyword here
+  const CurrencyConverterMaterialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(223, 28, 52, 72),
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        width: 2.0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(40),
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.grey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
-                  fontSize: 120,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(190, 132, 120, 120)),
+                fontSize: 120,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(190, 132, 120, 120),
+              ),
             ),
-            TextField(
-              style: TextStyle(
-                color: Colors.black,
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Enter amount in USD',
+                  hintStyle: TextStyle(
+                    color: Color.fromARGB(244, 158, 156, 156),
+                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
               ),
-              decoration: InputDecoration(
-                hintText: 'Enter amount in USD',
-                hintStyle: TextStyle(color: Color.fromARGB(244, 158, 156, 156)),
-                prefixIcon: Icon(Icons.monetization_on_outlined),
-                prefixIconColor: Colors.black,
-                filled: true,
-                fillColor: Colors.white,
-                focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      width: 2.0,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(60),
-                    )),
-                enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                  width: 2.0,
-                  style: BorderStyle.solid,
-                )),
-              ),
-              keyboardType: TextInputType.numberWithOptions(
-                decimal: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: TextButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('Pressed');
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: WidgetStateProperty.all(
+                    Colors.black,
+                  ),
+                  foregroundColor: WidgetStateProperty.all(
+                    Colors.white,
+                  ),
+                ),
+                child: const Text('Convert'),
               ),
             ),
           ],
